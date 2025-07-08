0d8311353623bd005d10979797817203752c7505
Udayanga2151 <deslakshan2151@gmail.com>
2025-07-08 14:27:45 +0530

    lblBygetElementmethod

---
+++ app.js
@@ -0,0 +1,8 @@
+function clickAction() {
+    let lblH2 = document.getElementById("h2lbl")
+    lblH2 .innerText="Lakshaan Cliked Submit button";
+    console.log(lblH2);
+    
+}  
+
+document.writeln("<br> documentwrite()  method calling!!!!");
